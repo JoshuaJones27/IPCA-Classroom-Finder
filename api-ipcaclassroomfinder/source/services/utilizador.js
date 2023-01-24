960ed21de4c0f6da70ef0f512bcef7f3b9506403
JoshuaJones27 <100417295+JoshuaJones27@users.noreply.github.com>
2023-01-24 12:22:52 +0000

    Será?

--- api-ipcaclassroomfinder/source/services/utilizador.js
+++ api-ipcaclassroomfinder/source/services/utilizador.js
@@ -29,9 +29,7 @@ module.exports = (app) => {
 
     const forgotPassword = async (filter) => {
         if (!filter.email) throw new ValidationError('O email é um campo obrigatório');
-        if (!filter.password) throw new ValidationError('A password é um campo obrigatório');
-        if (!filter.confirmpassword) throw new ValidationError('A password é um campo obrigatório');
-        if (filter.password !== filter.confirmpassword) throw new ValidationError('As password têm que coincidir');
+        if (!filter.palavraPasse) throw new ValidationError('A password é um campo obrigatório');
         if (!emailRegex.test(filter.email)) throw new ValidationError('O email não segue os padrões convencionais!');
     
         const result = await app.db('utilizadores').where('email', filter.email).first();
